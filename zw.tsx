3ab230fce6b6d544e0f9ef4c75289efea88a2b1a
QuaeDiciEade <72312078+QuaeDiciEade@users.noreply.github.com>
2020-10-23 11:13:36 +0200

    Technikum Created
    First Layer of Technikum. Test Version.
    No entrances, no links, no websides placed.

---
+++ zw.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="zw" tilewidth="32" tileheight="32" tilecount="2048" columns="32">
+ <image source="../ds2020workadventuretest/zw-map/tiles/zw.png" width="1024" height="2048"/>
+</tileset>
